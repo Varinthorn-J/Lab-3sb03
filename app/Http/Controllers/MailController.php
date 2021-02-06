ecd2ec2c2931188e359dd9845f794416aff42032
warinthon30 <jaitrong3016@gmail.com>
2021-02-06 23:07:03 +0700

    Start Use Git

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers; 
+
+use App\Models\lain;
+use Illuminate\Http\Request;
+use Mail;
+
+
+class MailController extends Controller
+
+{
+    public function basic_email(){
+        $data = array('name'=>"Warinthon");
+
+        Mail::send(['text'=>'mail'], $data, function($message){
+            $message->to('s6135512016@phuket.psu.ac.th','test')->subject('Laravel Basic Testing Mail');
+            $message->from('s6135512053@phuket.psu.ac.th','Warinthon');
+        });
+        echo "Basic Email Sent.Check your inbox.";
+    } 
+}
