53fccb9b919fd1b95f067473927afff1830a53ee
warinthon30 <jaitrong3016@gmail.com>
2021-02-06 23:27:57 +0700

    push

--- resources/views/index.php
+++ resources/views/index.php
@@ -10,7 +10,6 @@
 
 <?php
 
-
 // define variables and set to empty values
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
@@ -20,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nameErr = "Name is required";
   } else {
     $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
+    
     if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
       $nameErr = "Only letters and white space allowed";
     }
