0c71c823e04800189d87f3f5297d65c722c1b8ac
warinthon30 <jaitrong3016@gmail.com>
2021-02-07 13:13:36 +0700

    first commit

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -1,21 +1,27 @@
 <?php
 
-namespace App\Http\Controllers; 
+namespace App\Http\Controllers;
 
 use App\Models\lain;
 use Illuminate\Http\Request;
 use Mail;
 
-
 class MailController extends Controller
-
 {
-    public function basic_email(){
-        $data = array('name'=>"Warinthon Jaitrong");
-        Mail::send(['text'=>'mail'], $data, function($message){
-            $message->to('s6135512016@phuket.psu.ac.th','test')->subject('Laravel Basic Testing Mail');
-            $message->from('s6135512053@phuket.psu.ac.th','Warinthon');
+    public function basic_email()
+    {
+        $data = array('name' => $_GET['name'] , 'datamessage' => $_GET['message']);
+       
+        Mail::send(['text' => 'mail'], $data,function ($message) {
+            $message->to($_GET['email'],$_GET['name']);
+            $message->from('s6135512053@phuket.psu.ac.th', 'Warinthon');
         });
-        echo "Basic Email Sent.Check your inbox.";
-    } 
+        echo "Basic Email Sent. Check your inbox.";
+        echo "<br>";
+        echo "Name : " . $_GET['name'];
+        echo "<br>";
+        echo "Email : " . $_GET['email'];
+        echo "<br>";
+        echo "Message : " . $_GET['message'];
+    }
 }
