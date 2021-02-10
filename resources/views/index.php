6ad633be924ffab3d00c42f1f51a8976a028e7ab
warinthon30 <jaitrong3016@gmail.com>
2021-02-10 15:09:18 +0700

    commit3

--- resources/views/index.php
+++ resources/views/index.php
@@ -2,18 +2,20 @@
 <!DOCTYPE HTML>  
 <html>
 <head>
-
+  <h2 style="color: rgb(0, 0, 0);">6135512053</h2>
 </head>
-    <body>  
-<h2>6135512053</h2>
-<form name="form1" method="get" action="/sendbasicemail">  
-  Name: <input type="text" name="name"  placeholder="name" >
+      
+<body style="background-color:rgb(192, 176, 202);text-align:center; padding: 40px;">  
+
+<form  name="form1" method="get" action="/sendbasicemail">  
+  <h2 style="text-align: center;color:rgb(136, 15, 218)">basicemail</h2>
+  Name: <input  type="text" name="name"  placeholder="name" >
   <br><br>
   E-mail: <input type="email" name="email"  placeholder="email" >
   <br><br>
   Message <input type="text" name="message"  placeholder="message" >
   <br><br>
-  <input type="submit" name="submit" value="Submit">  
+  <input style="text-align: center;" type="submit" name="submit" value="Submit">  
 
 </form>
     </body>
